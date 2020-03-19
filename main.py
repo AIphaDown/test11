f26882c52facce66409eb4bb58a0daeca86d1f6a
Wonntann <wonntann@users.noreply.github.com>
2020-03-18 17:27:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 pygame.init()
 
 screen_info = pygame.display.Info()
-screen_size = (screen_info.current_w, screen_info.current_h)
+# screen_size = (screen_info.current_w, screen_info.current_h)
 
 # set size
 size = (width, height) = (850, 480)
@@ -14,16 +14,16 @@ clock = pygame.time.Clock()
 color = (98, 26, 255)
 
 # ball
-ball_image = pygame.image.load("ball.png")
-ball_image = pygame.transform.smoothscale(ball_image, (30, 30))
-ball_rect = ball_image.get_rect()
-ball_rect.center = (width//2, height//2)
-speed = pygame.math.Vector2(0, 5)
-speed.rotate_ip(random.randint(0, 360))
+balls = []
+# ball_image = pygame.image.load("ball.png")
+# ball_image = pygame.transform.smoothscale(ball_image, (30, 30))
+# ball_rect = ball_image.get_rect()
+# ball_rect.center = (width//2, height//2)
+# speed = pygame.math.Vector2(0, 5)
+# speed.rotate_ip(random.randint(0, 360))
 
-def move_ball():
-    ball_rect.move_ip(speed)
-
+# 3/25/20: call Class Ball in main function
+##########################################
 def main():
     while True:
         clock.tick(60)
